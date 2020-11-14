805ee04761f9e690a7fd5bb558edf262fecc1a86
wayback-home <dudtj4329@gmail.com>
2020-11-14 16:30:05 +0900

    hi

--- c/C/1학년 2학기/chapter 4/프로그래밍 과제 10.c
+++ c/C/1학년 2학기/chapter 4/프로그래밍 과제 10.c
@@ -10,7 +10,7 @@ int main(void)
     unsigned int byte0 = 0x000000ff;
     unsigned int byte1 = 0x0000ff00;
     unsigned int byte2 = 0x00ff0000;
-    unsigned int byte3 = 0xff000000;
+    unsigned int byte3 =   0xff000000;
 
     printf("byte 0 : %x\n", getData & byte0);
     printf("byte 1 : %x\n", ((getData & byte1) >> 8));
