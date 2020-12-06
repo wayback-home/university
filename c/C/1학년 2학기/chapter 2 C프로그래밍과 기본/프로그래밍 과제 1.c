e3e655c875f5004632e2b7b66b7cd504d4a1baec
wayback-home <dudtj4329@gmail.com>
2020-12-06 23:04:31 +0900

    remove exefiles

--- c/C/1학년 2학기/chapter 2 C프로그래밍과 기본/프로그래밍 과제 1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(void)
-{
-    printf("int main(void)\n{\n    return 0;\n}");
-
-    return 0;
-}
--- python/1-2 컴퓨터와 코딩/12주차 수업/dictionary2.py
+++
@@ -1,8 +0,0 @@
-a_dict = {}
-
-a_dict["one"]: "하나"
-a_dict["two"]: "둘"
-a_dict["three"]: "셋"
-
-word = input("단어를 입력하세요 :")
-print(a_dict[word])
