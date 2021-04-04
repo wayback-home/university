b721f1a3d18b7fea3f52e31f71c07d22a6a99dd9
dudtj <dudtj4329@gmail.com>
2021-04-04 18:50:10 +0900

    객체지향프로그래밍 과제 완성

--- python/학교/2-1 객체지향프로그래밍/5주차/과제/menu.py
+++ python/학교/2-1 객체지향프로그래밍/5주차/과제/menu.py
@@ -5,38 +5,17 @@ import time
 def printMenu():
     print(
         """
-    \t\t\t1. 더하기
-    \t\t\t2. 빼기
-    \t\t\t3. 곱하기
-    \t\t\t4. 나누기
-    \t\t\t5. 몫 구하기
-    \t\t\t6. 나머지 구하기
-    \t\t\t7. 제곱 구하기
-    \t\t\t8. 종료
+    \t\t\t1. 더하기  2. 빼기  3. 곱하기  4. 나누기  5. 몫 구하기
+    \t\t\t6. 나머지 구하기  7. 제곱 구하기  8. 종료
         """
     )
-    print("\n")
-    print("\t\t사용할 기능을 선택해주세요.")
     print("\n")
 
 
 def getMenuNumber():
-    while 1:
-        getmenu = int(input())
-        if getmenu not in [1, 2, 3, 4, 5, 6, 7, 8]:
-            print("\t\t잘못 입력하셨습니다. 1에서 8 사이의 숫자를 입력해주세요")
-            printMenu()
-
-        else:
-            print("\n\n")
-            print("\t\t선택한 기능을 실행합니다", end="")
-            time.sleep(1)
-            print(".", end="")
-            time.sleep(1)
-            print(".", end="")
-            time.sleep(1)
-            print(".")
-            time.sleep(1)
-            print("\n" * 30)
-            return getmenu
-            break
+    menuNumber = input("\t\t원하는 번호를 선택하세요 : ")
+    while menuNumber not in ["1", "2", "3", "4", "5", "6", "7", "8"]:
+        print("\t\t잘못 입력하셨습니다. 다시 입력해주세요")
+        print("\n\n")
+        menuNumber = input("\t\t원하는 번호를 선택하세요 : ")
+    return menuNumber
--- python/학교/2-1 객체지향프로그래밍/5주차/과제/최영서(1826052)-계산기-3반-2.py
+++ python/학교/2-1 객체지향프로그래밍/5주차/과제/최영서(1826052)-계산기-3반-2.py
@@ -2,6 +2,7 @@
 import sys
 import calculator
 import menu
+import time
 
 
 def getOperand():
@@ -14,11 +15,12 @@ def getOperand():
         elif number1.isdecimal:
             number1 = float(number1)
             break
+
         else:
             print("\t\t잘못 입력하셨습니다. 다시 입력해주세요.")
 
     while 1:
-        number2 = input("\t\t첫번째 숫자를 입력하세요 : ")
+        number2 = input("\t\t두번째 숫자를 입력하세요 : ")
         if number2.isdecimal:
             number2 = int(number2)
             break
@@ -26,38 +28,38 @@ def getOperand():
         elif number2.isdecimal:
             number2 = float(number2)
             break
+
         else:
             print("\t\t잘못 입력하셨습니다. 다시 입력해주세요.")
     return number1, number2
 
 
 def printResult(op1, op2):
-    while menu.getMenuNumber() != 8:
-        if menu.getMenuNumber() == 1:
-            print(f"\t\t{op1} + {op2} = {calculator.plus(op1, op2)}")
+    if menunumber == "1":
+        print("\t\t", op1, "+", op2, "=", calculator.plus(op1, op2))
 
-        elif menu.getMenuNumber() == 2:
-            print(f"\t\t{op1} - {op2} = {calculator.minus(op1, op2)}")
+    elif menunumber == "2":
+        print("\t\t", op1, "-", op2, "=", calculator.minus(op1, op2))
 
-        elif menu.getMenuNumber() == 3:
-            print(f"\t\t{op1} * {op2} = {calculator.multi(op1, op2)}")
+    elif menunumber == "3":
+        print("\t\t", op1, "*", op2, "=", calculator.multi(op1, op2))
 
-        elif menu.getMenuNumber() == 4:
-            print(f"\t\t{op1} / {op2} = {calculator.divide(op1, op2)}")
+    elif menunumber == "4":
+        print("\t\t", op1, "/", op2, "=", calculator.divide(op1, op2))
 
-        elif menu.getMenuNumber() == 5:
-            print(f"\t\t{op1} // {op2} = {calculator.quotient(op1, op2)}")
+    elif menunumber == "5":
+        print("\t\t", op1, "//", op2, "=", calculator.quotient(op1, op2))
 
-        elif menu.getMenuNumber() == 6:
-            print(f"\t\t{op1} % {op2} = {calculator.remainder(op1, op2)}")
+    elif menunumber == "6":
+        print("\t\t", op1, "%", op2, "=", calculator.remainder(op1, op2))
 
-        elif menu.getMenuNumber() == 7:
-            print(f"\t\t{op1} ** {op2} = {calculator.square(op1, op2)}")
+    elif menunumber == "7":
+        print("\t\t", op1, "**", op2, "=", calculator.square(op1, op2))
 
 
 if len(sys.argv) == 2 and sys.argv[1] == "/?":
     print("\n" * 30)
-    print("\t\t이 프로그램은 계산기입니다.")
+    print("\t\t이 프로그램은 두가지 인수를 받아 계산값을 출력합니다.")
     print("\t\t실행 가능한 동작 목록")
     print(
         """
@@ -71,16 +73,24 @@ if len(sys.argv) == 2 and sys.argv[1] == "/?":
     \t\t\t6. 종료
     """
     )
-    print("\n\n")
 
 else:
-    while menu.getMenuNumber != "8":
-        menu.printMenu()
+    menu.printMenu()
+    menunumber = menu.getMenuNumber()
 
+    while menunumber != "8":
+        operand = getOperand()
+
+        while menunumber == "4" and operand[1] == 0:
+            print("0으로 나눌 수 없습니다. 다시입력해주세요")
+            operand = getOperand()
+
+        printResult(operand[0], operand[1])
+        time.sleep(3)
+
+        menu.printMenu()
         menunumber = menu.getMenuNumber()
 
-        operand = getOperand()
-        printResult(operand[0], operand[1])
-
+    print("\n" * 30)
     print("\t\t종료합니다.")
     print("\n")
