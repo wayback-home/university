00e597ff1d0ae4fef71c1f8a776e7eb5a38cc831
dudtj <dudtj4329@gmail.com>
2021-04-10 17:37:33 +0900

    embeded Homework week6

--- c/C/2학년 1학기/week6HW.c
+++ c/C/2학년 1학기/week6HW.c
@@ -13,171 +13,156 @@ int main(void)
     PORTF = 0x0F;
 
     int keyinput;
+
     while (1)
     {
+        //40 50 60 70
 
-        while (1)
+        PORTF = 0x01;
+        _delay_ms(5);
+        keyinput = PINF;
+
+        if (keyinput == 0x11)
         {
-            //40 50 60 70
-            PORTF = 0x01;
+            PORTE = led[0];
             _delay_ms(5);
-            keyinput = PINF;
+            keyinput = 0x00;
+            PORTE = 0xF0;
+        }
 
-            if (keyinput == 0x11)
-            {
-                PORTE = led[0];
-                _delay_ms(5);
-                keyinput = 0x00;
-                PORTE = 0xF0;
-                break;
-            }
-
-            else if (keyinput == 0x21)
-            {
-                PORTE = led[4];
-                _delay_ms(5);
-                keyinput = 0x00;
-                PORTE = 0xF0;
-                break;
-            }
-
-            else if (keyinput == 0x41)
-            {
-                PORTE = led[8];
-                _delay_ms(5);
-                keyinput = 0x00;
-                PORTE = 0xF0;
-                break;
-            }
-
-            else if (keyinput == 0x81)
-            {
-                PORTE = led[12];
-                _delay_ms(5);
-                keyinput = 0x00;
-                PORTE = 0xF0;
-                break;
-            }
-
-            //41 51 61 71
-            PORTF = 0x02;
+        else if (keyinput == 0x21)
+        {
+            PORTE = led[4];
             _delay_ms(5);
+            keyinput = 0x00;
+            PORTE = 0xF0;
+        }
 
-            if (keyinput == 0x12)
-            {
-                PORTE = led[1];
-                _delay_ms(5);
-                keyinput = 0x00;
-                PORTE = 0xF0;
-                break;
-            }
-
-            else if (keyinput == 0x22)
-            {
-                PORTE = led[5];
-                _delay_ms(5);
-                keyinput = 0x00;
-                PORTE = 0xF0;
-                break;
-            }
-
-            else if (keyinput == 0x42)
-            {
-                PORTE = led[9];
-                _delay_ms(5);
-                keyinput = 0x00;
-                PORTE = 0xF0;
-                break;
-            }
-
-            else if (keyinput == 0x82)
-            {
-                PORTE = led[13];
-                _delay_ms(5);
-                keyinput = 0x00;
-                PORTE = 0xF0;
-                break;
-            }
-
-            //42 52 62 72
-            PORTF = 0x04;
+        else if (keyinput == 0x41)
+        {
+            PORTE = led[8];
             _delay_ms(5);
+            keyinput = 0x00;
+            PORTE = 0xF0;
+        }
 
-            if (keyinput == 0x14)
-            {
-                PORTE = led[2];
-                _delay_ms(5);
-                keyinput = 0x00;
-                PORTE = 0xF0;
-                break;
-            }
-
-            else if (keyinput == 0x24)
-            {
-                PORTE = led[6];
-                _delay_ms(5);
-                keyinput = 0x00;
-                PORTE = 0xF0;
-                break;
-            }
-
-            else if (keyinput == 0x44)
-            {
-                PORTE = led[10];
-                _delay_ms(5);
-                keyinput = 0x00;
-                PORTE = 0xF0;
-                break;
-            }
-
-            else if (keyinput == 0x84)
-            {
-                PORTE = led[14];
-                _delay_ms(5);
-                keyinput = 0x00;
-                PORTE = 0xF0;
-                break;
-            }
-
-            //43 53 63 73
-            PORTF = 0x08;
+        else if (keyinput == 0x81)
+        {
+            PORTE = led[12];
             _delay_ms(5);
+            keyinput = 0x00;
+            PORTE = 0xF0;
+        }
 
-            if (keyinput == 0x18)
-            {
-                PORTE = led[3];
-                _delay_ms(5);
-                keyinput = 0x00;
-                PORTE = 0xF0;
-                break;
-            }
+        //41 51 61 71
+        PORTF = 0x02;
+        _delay_ms(5);
+        keyinput = PINF;
 
-            else if (keyinput == 0x28)
-            {
-                PORTE = led[7];
-                _delay_ms(5);
-                keyinput = 0x00;
-                PORTE = 0xF0;
-                break;
-            }
+        if (keyinput == 0x12)
+        {
+            PORTE = led[1];
+            _delay_ms(5);
+            keyinput = 0x00;
+            PORTE = 0xF0;
+        }
 
-            else if (keyinput == 0x48)
-            {
-                PORTE = led[11];
-                _delay_ms(5);
-                keyinput = 0x00;
-                PORTE = 0xF0;
-                break;
-            }
+        else if (keyinput == 0x22)
+        {
+            PORTE = led[5];
+            _delay_ms(5);
+            keyinput = 0x00;
+            PORTE = 0xF0;
+        }
 
-            else if (keyinput == 0x88)
-            {
-                PORTE = led[15];
-                _delay_ms(5);
-                keyinput = 0x00;
-                PORTE = 0xF0;
-                break;
-            }
+        else if (keyinput == 0x42)
+        {
+            PORTE = led[9];
+            _delay_ms(5);
+            keyinput = 0x00;
+            PORTE = 0xF0;
+        }
+
+        else if (keyinput == 0x82)
+        {
+            PORTE = led[13];
+            _delay_ms(5);
+            keyinput = 0x00;
+            PORTE = 0xF0;
+        }
+
+        //42 52 62 72
+        PORTF = 0x04;
+        _delay_ms(5);
+        keyinput = PINF;
+
+        if (keyinput == 0x14)
+        {
+            PORTE = led[2];
+            _delay_ms(5);
+            keyinput = 0x00;
+            PORTE = 0xF0;
+        }
+
+        else if (keyinput == 0x24)
+        {
+            PORTE = led[6];
+            _delay_ms(5);
+            keyinput = 0x00;
+            PORTE = 0xF0;
+        }
+
+        else if (keyinput == 0x44)
+        {
+            PORTE = led[10];
+            _delay_ms(5);
+            keyinput = 0x00;
+            PORTE = 0xF0;
+        }
+
+        else if (keyinput == 0x84)
+        {
+            PORTE = led[14];
+            _delay_ms(5);
+            keyinput = 0x00;
+            PORTE = 0xF0;
+        }
+
+        //43 53 63 73
+        PORTF = 0x08;
+        _delay_ms(5);
+        keyinput = PINF;
+
+        if (keyinput == 0x18)
+        {
+            PORTE = led[3];
+            _delay_ms(5);
+            keyinput = 0x00;
+            PORTE = 0xF0;
+        }
+
+        else if (keyinput == 0x28)
+        {
+            PORTE = led[7];
+            _delay_ms(5);
+            keyinput = 0x00;
+            PORTE = 0xF0;
+        }
+
+        else if (keyinput == 0x48)
+        {
+            PORTE = led[11];
+            _delay_ms(5);
+            keyinput = 0x00;
+            PORTE = 0xF0;
+        }
+
+        else if (keyinput == 0x88)
+        {
+            PORTE = led[15];
+            _delay_ms(5);
+            keyinput = 0x00;
+            PORTE = 0xF0;
         }
     }
 
