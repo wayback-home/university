a7da5316da4e2fc588f990ce80958ba5cb2c058b
dudtj <dudtj4329@gmail.com>
2021-10-04 22:29:04 +0900

    windows programming week5 homework

---
+++ csharp/week5/07/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _07
+{
+    class Program
+    {
+
+
+        public class Person
+        {
+            public string name = "윤인성";
+            public string address = "서울특별시 강서구";
+            
+            return name
+            return address
+            Pet()
+        }
+
+        public class Pet
+        {
+            public string name {get;set;}
+            public int age {get; set;}
+
+            public string returnPet()
+            {
+                return "이름 : " + name + "\t나이 : " + age;
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+    
+            List<Pet> pets = new List<Pet>();
+            pets.Add(new Pet() {name="구름", age=7});
+            pets.Add(new Pet() {name="별", age=1});
+
+
+            Console.WriteLine(Person());
+        }
+    }
+}
---
+++ csharp/week5/08/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int num = random.Next(0,1000);
+
+            
+            int a;
+            while (true)
+            {
+                Console.WriteLine("숫자를 입력해주세요 : ");
+                string sNum = Console.ReadLine();
+                a = Convert.ToInt32(sNum);
+                if (a == num)
+                {
+                    Console.WriteLine("맞췄습니다.");
+                    break;
+                }
+                else if (a>num)
+                {
+                    Console.WriteLine("더 작습니다.");
+                }
+                else if (a<num)
+                {
+                    Console.WriteLine("더 큽니다.");
+                }
+            }
+
+        }
+    }
+}
