c5661ff129c10e9dde9dda41daad28cea923004c
dudtj <dudtj4329@gmail.com>
2021-11-11 14:02:42 +0900

    csharp week 11

---
+++ csharp/week11/pr1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace pr1
+{
+    class Program
+    {
+        struct PointD{
+            public int x;
+            public int y;
+            public string testA;
+            public string testB;
+
+            public PointD(int x, int y){
+                this.x = x;
+                this.y = y;
+                this.testA = "초기화";
+                this.testB = "초기화";
+
+            }
+            
+            public PointD(int x, int y, string test){
+                this.x = x;
+                this.y = y;
+                this.testA = test;
+                this.testB = test;
+            }
+        }
+        static void Main(string[] args)
+        {
+            PointD pd = new PointD();
+            // PointD pd = new PointD(10, 20);
+            // PointD pd = new PointD(10, 20, "테스트");
+            Console.WriteLine(pd.x + ","+pd.y);
+            Console.WriteLine(pd.testA + ","+pd.testB);
+        }
+    }
+}
---
+++ csharp/week11/pr2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace pr2
+{
+    class Program
+    {
+        class PointClass{
+            public int x;
+            public int y;
+
+            public PointClass(int x, int y){
+                this.x = x;
+                this.y = y;
+            }
+
+        }
+        struct PointStruct{
+            public int x;
+            public int y;
+
+            public PointStruct(int x, int y){
+                this.x = x;
+                this.y = y;
+            }
+        }
+        static void Main(string[] args) //! 활용1
+        {
+            PointClass pointClassA = new PointClass(10, 20); 
+            PointClass pointClassB = pointClassA;
+
+            Console.WriteLine("pointClassA : " + pointClassA.x + ", "+pointClassA.y);
+            Console.WriteLine("pointClassB : " + pointClassB.x + ", "+pointClassB.y);
+
+            PointStruct pointStructA = new PointStruct(10, 20);
+            PointStruct pointStructB = pointStructA;
+
+            Console.WriteLine("pointStructA : " + pointStructA.x + ", "+pointStructA.y);
+            Console.WriteLine("pointStructB : " + pointStructB.x + ", "+pointStructB.y);
+
+
+        }
+
+        /* static void Main(string[] args){ //! 활용2
+            PointClass pointClassA = new PointClass(10,20);
+            PointClass pointClassB = pointClassA;
+
+            pointClassB.x = 100;
+            pointClassB.y = 200;
+
+            Console.WriteLine("pointClassA : "+pointClassA.x +", "+pointClassA.y );
+            Console.WriteLine("pointClassB : "+pointClassB.x +", "+pointClassB.y );
+
+
+            PointStruct pointStructA = new PointStruct(10, 20);
+            PointStruct pointStructB = pointStructA;
+
+            pointStructB.x = 100;
+            pointStructB.y = 200;
+
+            Console.WriteLine("pointStructA : "+pointStructA.x +", "+pointStructA.y );
+            Console.WriteLine("pointStructB : "+pointStructB.x +", "+pointStructB.y );
+        } */
+    }
+}
