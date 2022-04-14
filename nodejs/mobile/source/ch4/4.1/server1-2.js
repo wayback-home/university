730c8e60d3263c4b17c82fdb1c0a6a3af983e576
wayback-home <dudtj4329@gmail.com>
2022-04-14 14:52:12 +0900

    웹 프로그래밍 응용 수업

--- nodejs/mobile/source/ch4/4.1/server1-2.js
+++ nodejs/mobile/source/ch4/4.1/server1-2.js
@@ -3,17 +3,64 @@ const http = require('http');
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
   res.write('<h1>Hello Node!</h1>');
+  res.write('<h3>8080 Port</h3>');
   res.end('<p>Hello Server!</p>');
 })
   .listen(8080, () => { // 서버 연결
     console.log('8080번 포트에서 서버 대기 중입니다!');
   });
 
+
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
   res.write('<h1>Hello Node!</h1>');
+  res.write('<h3>8081 Port</h3>');
   res.end('<p>Hello Server!</p>');
 })
-  .listen(8080, () => { // 서버 연결
+  .listen(8081, () => { // 서버 연결
     console.log('8081번 포트에서 서버 대기 중입니다!');
+  });
+
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.write('<h1>Hello Node!</h1>');
+  res.write('<h3>8082 Port</h3>');
+  res.end('<p>Hello Server!</p>');
+})
+  .listen(8082, () => { // 서버 연결
+    console.log('8082번 포트에서 서버 대기 중입니다!');
+  });
+
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.write('<h1>Hello Node!</h1>');
+  res.write('<h3>8083 Port</h3>');
+  res.end('<p>Hello Server!</p>');
+})
+  .listen(8083, () => { // 서버 연결
+    console.log('8083번 포트에서 서버 대기 중입니다!');
+  });
+
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.write('<h1>Hello Node!</h1>');
+  res.write('<h3>8084 Port</h3>');
+  res.end('<p>Hello Server!</p>');
+})
+  .listen(8084, () => { // 서버 연결
+    console.log('8084번 포트에서 서버 대기 중입니다!');
+  });
+
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.write('<h1>Hello Node!</h1>');
+  res.write('<h3>8085 Port</h3>');
+  res.end('<p>Hello Server!</p>');
+})
+  .listen(8085, () => { // 서버 연결
+    console.log('8085번 포트에서 서버 대기 중입니다!');
   });
--- nodejs/mobile/source/ch4/4.2/restServer.js
+++ nodejs/mobile/source/ch4/4.2/restServer.js
@@ -17,6 +17,10 @@ http.createServer(async (req, res) => {
       } else if (req.url === '/users') {
         res.writeHead(201, { 'Content-Type': 'application/json; charset=utf-8' });
         return res.end(JSON.stringify(users));
+      } else if (req.url === '/contact'){
+        const data = await fs.readFile('./contact.html');
+        res.writeHead(200, {'Content-Type' : 'text/html; charset= utf-8'});
+        return res.end(data);
       }
       // /도 /about도 /users도 아니면
       try {
