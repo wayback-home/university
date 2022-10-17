e6d5f47fa2046d43deaee9b4dbff0ea82b302e11
wayback-home <dudtj4329@gmail.com>
2022-10-17 21:47:15 +0900

    데이터과학 공공API 오류수정

--- python/학교/3-2 데이터과학/week3/openAPI.py
+++ python/학교/3-2 데이터과학/week3/openAPI.py
@@ -1,18 +1,17 @@
-import os
-import sys
-import urllib.request
 import datetime
-import time
 import json
+import urllib.request
 import pandas as pd
 
 ServiceKey = "HPGTaDn%2FOaGT8JrPDJ9LlcJCALn7QpFFU4LPiv5zwIJtnwM06IijGWhSTpi7T4yLu%2BsWjiBR0ioPJ%2BuoKVdv8Q%3D%3D"
 
+
 # [CODE 1]
-def getRequestUrl(url):
-    req = urllib.request.Request(url)
+def get_request_url(url):
+    request = urllib.request.Request(url)
+
     try:
-        response = urllib.request.urlopen(req)
+        response = urllib.request.urlopen(request)
         if response.getcode() == 200:
             print("[%s] Url Request Success" % datetime.datetime.now())
             return response.read().decode("utf-8")
@@ -23,7 +22,7 @@ def getRequestUrl(url):
 
 
 # [CODE 2]
-def getTourismStatsItem(yyyymm, national_code, ed_cd):
+def get_tourism_stats_item(yyyymm, national_code, ed_cd):
     service_url = "http://openapi.tour.go.kr/openapi/service/EdrcntTourismStatsService/getEdrcntTourismStatsList"
 
     parameters = "?_type=json&serviceKey=" + ServiceKey  # 인증키
@@ -31,71 +30,106 @@ def getTourismStatsItem(yyyymm, national_code, ed_cd):
     parameters += "&NAT_CD=" + national_code
     parameters += "&ED_CD=" + ed_cd
 
+    url = service_url + parameters
+    print(url)  # 액세스 거부 여부 확인용 출력
 
-def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
-    jsonResult = []
+    # [CODE 1]
+    result_data = get_request_url(url)
+
+    if result_data is None:
+        return None
+    else:
+        return json.loads(result_data)
+
+
+# [CODE 3]
+def get_tourism_stats_service(nat_cd, ed_cd, start_year, end_year):
+    json_result = []
     result = []
-    for year in range(nStartYear, nEndYear + 1):
+    nat_name = ""
+    ed = ""
+    data_end = ""
+
+    for year in range(start_year, end_year + 1):
         for month in range(1, 13):
             yyyymm = "{0}{1:0>2}".format(str(year), str(month))
-            jsonData = getTourismStatsItem(yyyymm, nat_cd, ed_cd)  # [CODE 2]
-            if jsonData["response"]["header"]["resultMsg"] == "OK":
+            json_data = get_tourism_stats_item(yyyymm, nat_cd, ed_cd)  # [CODE 2]
+
+            if json_data["response"]["header"]["resultMsg"] == "OK":
                 # 데이터가 없는 마지막 항목인 경우 ----------------------------
-                if jsonData["response"]["body"]["items"] == "":
-                    dataEND = "{0}{1:0>2}".format(str(year), str(month - 1))
+                if json_data["response"]["body"]["items"] == "":
+                    data_end = "{0}{1:0>2}".format(str(year), str(month - 1))
+                    print("data_end = ", data_end)
                     print(
-                        "데이터 없음.... \n 제공되는 통계 데이터는 %s년   %s월까지입니다."
+                        "데이터 없음.... \n 제공되는 통계 데이터는 %s년 %s월까지 입니다."
                         % (str(year), str(month - 1))
                     )
                     break
-            # jsonData를 출력하여 확인...........................................
-            print(json.dumps(jsonData, indent=4, sort_keys=True, ensure_ascii=False))
-            natName = jsonData["response"]["body"]["items"]["item"]["natKorNm"]
-            natName = natName.replace(" ", "")
-            num = jsonData["response"]["body"]["items"]["item"]["num"]
-            ed = jsonData["response"]["body"]["items"]["item"]["ed"]
-            print("[ %s_%s : %s ]" % (natName, yyyymm, num))
-            print("-----------------------------------------------------")
-            jsonResult.append(
-                {
-                    "nat_name": natName,
-                    "nat_cd": nat_cd,
-                    "yyyymm": yyyymm,
-                    "visit_cnt": num,
-                }
-            )
-            result.append([natName, nat_cd, yyyymm, num])
-    return (jsonResult, result, natName, ed, dataEND)
+
+                # json_data를 출력하여 확인...........................................
+                print(
+                    json.dumps(json_data, indent=4, sort_keys=True, ensure_ascii=False)
+                )
+
+                nat_name = json_data["response"]["body"]["items"]["item"]["natKorNm"]
+                nat_name = nat_name.replace(" ", "")
+                num = json_data["response"]["body"]["items"]["item"]["num"]
+                ed = json_data["response"]["body"]["items"]["item"]["ed"]
+
+                print("[ %s_%s : %s ]" % (nat_name, yyyymm, num))
+                print("-----------------------------------------------------")
+                json_result.append(
+                    {
+                        "nat_name": nat_name,
+                        "nat_cd": nat_cd,
+                        "yyyymm": yyyymm,
+                        "visit_cnt": num,
+                    }
+                )
+
+                result.append([nat_name, nat_cd, yyyymm, num])
+
+    # TODO UnboundLocalError: local variable 'data_end' referenced before assignment 해결
+    return json_result, result, nat_name, ed, data_end
 
 
+# [CODE 0]
 def main():
-    jsonResult = []
-    result = []
-
-    print("<< 입국한 외국인의 통계 데이터를 수집합니다. >>")
+    # 요청변수(Request Parameter)
+    # get 국가코드
     nat_cd = input("국가 코드를 입력하세요(중국: 112 / 일본: 130 / 미국: 275) : ")
-    nStartYear = int(input("데이터를 몇 년부터 수집할까요? : "))
-    nEndYear = int(input("데이터를 몇 년까지 수집할까요? : "))
-    ed_cd = "E"  # E : 방한외래관광객, D : 해외 출국
-    jsonResult, result, natName, ed, dataEND = getTourismStatsService(
-        nat_cd, ed_cd, nStartYear, nEndYear
-    )  # [CODE 3]
+    start_year = int(input("데이터를 몇 년부터 수집할까요? : "))
+    end_year = int(input("데이터를 몇 년까지 수집할까요? : "))
+    # get 출입국구분코드, E = 방한외래관광객, D = 해외 출국
+    ed_cd = "E"
+
+    # [CODE 3]
+    json_result, result, nat_name, ed, data_end = get_tourism_stats_service(
+        nat_cd, ed_cd, start_year, end_year
+    )
 
     # 파일저장 1 : json 파일
     with open(
-        "./%s_%s_%d_%s.json" % (natName, ed, nStartYear, dataEND), "w", encoding="utf8"
+        "./%s_%s_%d_%s.json" % (nat_name, ed, start_year, data_end),
+        "w",
+        encoding="utf8",
     ) as outfile:
-        jsonFile = json.dumps(jsonResult, indent=4, sort_keys=True, ensure_ascii=False)
-        outfile.write(jsonFile)
+        json_file = json.dumps(
+            json_result, indent=4, sort_keys=True, ensure_ascii=False
+        )
+        outfile.write(json_file)
+
     # 파일저장 2 : csv 파일
     columns = ["입국자국가", "국가코드", "입국연월", "입국자 수"]
     result_df = pd.DataFrame(result, columns=columns)
     result_df.to_csv(
-        "./%s_%s_%d_%s.csv" % (natName, ed, nStartYear, dataEND),
+        "./%s_%s_%d_%s.csv" % (nat_name, ed, start_year, data_end),
         index=False,
-        encoding="cp949",
+        encoding="utf8",
     )
 
 
 if __name__ == "__main__":
+    print("한국교통대학교 컴퓨터공학과 1826047 조태현")
+    print("공공데이터포털, 한국문화관광연구원_출입국관광통계서비스")
     main()
